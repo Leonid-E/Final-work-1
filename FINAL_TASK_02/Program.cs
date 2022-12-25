e98c9ff10cdd830bdd85bcb16a7a1cb28352445c
Leonid_Erchenko <leonidercenko@MacBook-Air-Leonid.local>
2022-12-25 22:53:47 +0300

    Добавил файл с кодом проекта

---
+++ FINAL_TASK_02/Program.cs
@@ -0,0 +1,18 @@
+﻿// Блок-схема: Найти среднее арифметическое элементов массива, 
+// заполненного случайными целыми числами от 0 до 99. 
+// Размер массива задаёт пользователь. 
+
+Console.Write("Задайте размер массива: ");
+int size = Convert.ToInt32(Console.ReadLine());
+double [] arrey = new double[size];
+double sum = 0;
+Console.Write("Массив : ");
+Random rand = new Random();
+for (int i = 0; i < arrey.Length; i++)
+{
+    arrey[i] = new Random().Next(100);
+    sum += arrey[i]; 
+    Console.Write(arrey[i] + " ");
+}
+Console.WriteLine();
+Console.Write($"Среднее арефмитическое : {Math.Round((sum / size), 1)}");
